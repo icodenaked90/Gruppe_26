c5f7d85e6b632520e25ee7a8de13b255499fce64
simon <simonphilipsen1990@gmail.com>
2019-03-18 11:24:55 +0100

    client added

---
+++ vegt_Client/src/main/java/Client.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String hostName = "127.0.0.1";
+        int portNumber = 8000;
+
+
+        Socket echoSocket = new Socket(hostName, portNumber);
+        PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
