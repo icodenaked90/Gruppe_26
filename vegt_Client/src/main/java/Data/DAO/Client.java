2b6524c0ea35077d4984da9d51970d5a5867dd09
Mark Malloy <markmalloy96@yahoo.dk>
2019-03-22 17:38:18 +0100

    Program now runs without collecting or storing input, but shows entire program duration in the gui as a test to provide a better view

--- vegt_Client/src/main/java/Data/DAO/Client.java
+++ vegt_Client/src/main/java/Data/DAO/Client.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
+import Data.DTO.UserDTO;
+import Funktion.commController;
 // Det er meningen, at I kun bruger kommandoerne S, T, D, DW, P111 og RM20 8.
-
+@SuppressWarnings("Duplicates")
 public class Client implements IClientDAO {
 
     // Fields
@@ -16,36 +19,59 @@ public class Client implements IClientDAO {
 
     public Client() throws IOException,InterruptedException { // Midlertidig løsning
     }
+    // Initialization
+    Socket echoSocket = new Socket(hostName, portNumber); //Socket
+    PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
     // Midlertidig main til testing
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         try {
             Client client = new Client();
+            client.printOnTextDisplay("hello");
+            commController start = new commController();
+            // start.tui();
+            UserDTO user = new UserDTO();
 
+            Scanner scan = new Scanner(System.in);
             client.printOnTextDisplay("Indtast operatørnummer");
-            // Brugeren indtaster deres nummer. Brug evt. scanner til at modtage og gemme
-            client.printOnTextDisplay("Indtast batch nummer");
-            // Brugeren indtaster deres batchnummer med scanner
+            int userID = scan.nextInt();
+            if (userID >= 10 && userID < 100) {
+                int id = user.getId();
+                if (id == userID) {
+                    client.printOnTextDisplay("Operatør: " + user.getName());
+                    // Brugeren indtaster deres nummer. Brug evt. scanner til at modtage og gemme
+                    Thread.sleep(3000);
+                    client.printOnTextDisplay("Indtast batch nummer");
+                    int batchID = scan.nextInt();
+                    client.printOnTextDisplay("Vægten skal nu være ubelastet. Tryk ok for at godkende.");
+                    // Vægten tareres. Brugeren trykker enter.
+                    Thread.sleep(3000);
 
-            client.printOnTextDisplay("Vægten skal nu være ubelastet. Tryk ok for at godkende.");
-            // Vægten tareres. Brugeren trykker enter.
+                    client.printOnTextDisplay("Venligst placer beholder på vægten");
+                    // Registrer vægten af beholderen
+                    double tarWeight1 = client.getWeight(); // Vægten tareres
+                    Thread.sleep(3000);
 
-            client.printOnTextDisplay("Venligst placer beholder på vægten");
-            // Registrer vægten af beholderen
-            double tarWeight1 = client.getWeight(); // Vægten tareres
+                    // Vægten vejer nu med beholder og produkt
+                    client.printOnTextDisplay("Venligst placer beholderen med produktet på vægten");
+                    double nettoWeight = client.getWeight(); // Nettovægten fås
+                    Thread.sleep(3000);
 
-            // Vægten vejer nu med beholder og produkt
-            client.printOnTextDisplay("Venligst placer beholderen med produktet på vægten");
-            double nettoWeight = client.getWeight(); // Nettovægten fås
+                    // Vægten vejer nu uden noget på.
+                    client.printOnTextDisplay("Venligst fjern beholderen fra vægten");
+                    double bruttoWeight = client.getWeight(); // Bruttovægten fås
+                    Thread.sleep(3000);
 
-            // Vægten vejer nu uden noget på.
-            client.printOnTextDisplay("Venligst fjern beholderen fra vægten");
-            double bruttoWeight = client.getWeight(); // Bruttovægten fås
+                    client.printOnTextDisplay("Alt er kasseret ordenligt. Farvel.");
+                    // Vægten tareres igen.
 
-            client.printOnTextDisplay("Alt er kasseret ordenligt. Farvel.");
-            // Vægten tareres igen.
+                } else {
+                    client.printOnTextDisplay("Der findes ingen bruger med id" + userID + ". Prøv igen");
+                }
 
-
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -53,11 +79,6 @@ public class Client implements IClientDAO {
         }
     }
 
-    // Initialization
-    Socket echoSocket = new Socket(hostName, portNumber); //Socket
-    PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-    BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
 
     // Methods
@@ -81,13 +102,13 @@ public class Client implements IClientDAO {
         out.flush();
 
         String weightStr = in.readLine();
-        System.out.println("Ufiltreret String: "+weightStr);
+       // debug:  System.out.println("Ufiltreret String: "+weightStr);
 
 
         weightStr = weightStr.replaceAll("[^0-9.-]", "");
 
 
-        System.out.println("Filtreret String: "+weightStr);
+        // debug: System.out.println("Filtreret String: "+weightStr);
 
         double weight = Double.parseDouble(weightStr);
 
--- vegt_Client/src/main/java/Funktion/commController.java
+++ vegt_Client/src/main/java/Funktion/commController.java
@@ -15,29 +15,36 @@ public class commController{
             client.printOnTextDisplay("Indtast operatørnummer");
             int userID = scan.nextInt();
             if(userID>=10 && userID<100){
-
                 int id = user.getId();
-                client.printOnTextDisplay("Name");
-                // Brugeren indtaster deres nummer. Brug evt. scanner til at modtage og gemme
-                client.printOnTextDisplay("Indtast batch nummer");
-                int batchID = scan.nextInt();
-                client.printOnTextDisplay("Vægten skal nu være ubelastet. Tryk ok for at godkende.");
-                // Vægten tareres. Brugeren trykker enter.
+                if(id==userID){
+                    client.printOnTextDisplay("Operatør: "+user.getName());
+                    // Brugeren indtaster deres nummer. Brug evt. scanner til at modtage og gemme
+                    Thread.sleep(1000);
+                    client.printOnTextDisplay("Indtast batch nummer");
+                    int batchID = scan.nextInt();
+                    client.printOnTextDisplay("Vægten skal nu være ubelastet. Tryk ok for at godkende.");
+                    // Vægten tareres. Brugeren trykker enter.
 
-                client.printOnTextDisplay("Venligst placer beholder på vægten");
-                // Registrer vægten af beholderen
-                double tarWeight1 = client.getWeight(); // Vægten tareres
+                    client.printOnTextDisplay("Venligst placer beholder på vægten");
+                    // Registrer vægten af beholderen
+                    double tarWeight1 = client.getWeight(); // Vægten tareres
 
-                // Vægten vejer nu med beholder og produkt
-                client.printOnTextDisplay("Venligst placer beholderen med produktet på vægten");
-                double nettoWeight = client.getWeight(); // Nettovægten fås
+                    // Vægten vejer nu med beholder og produkt
+                    client.printOnTextDisplay("Venligst placer beholderen med produktet på vægten");
+                    double nettoWeight = client.getWeight(); // Nettovægten fås
 
-                // Vægten vejer nu uden noget på.
-                client.printOnTextDisplay("Venligst fjern beholderen fra vægten");
-                double bruttoWeight = client.getWeight(); // Bruttovægten fås
+                    // Vægten vejer nu uden noget på.
+                    client.printOnTextDisplay("Venligst fjern beholderen fra vægten");
+                    double bruttoWeight = client.getWeight(); // Bruttovægten fås
 
-                client.printOnTextDisplay("Alt er kasseret ordenligt. Farvel.");
-                // Vægten tareres igen.
+                    client.printOnTextDisplay("Alt er kasseret ordenligt. Farvel.");
+                    // Vægten tareres igen.
+                }
+                else{
+                    client.printOnTextDisplay("Der findes ingen bruger med id" + userID+". Prøv igen");
+                    tui();
+                }
+
             }
 
 
